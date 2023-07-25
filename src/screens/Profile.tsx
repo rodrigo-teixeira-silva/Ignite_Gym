a0256682a0cbf43889989dc273b177fc85a915a5
rodrigo teixeira silva <r.teixeira1@outlook.com.br>
2023-07-25 18:41:00 -0300

    rotas_da-aplicação

---
+++ src/screens/Profile.tsx
@@ -0,0 +1,10 @@
+import { Center, Text } from "native-base";
+
+export function Profile(){
+    return(
+        <Center flex={1}>
+        <Text color='white'>Profile</Text>
+        </Center>
+    );
+
+}
