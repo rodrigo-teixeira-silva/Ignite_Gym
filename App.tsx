628e097d9fbef28aa0d1581fc4a74cd90fb7b03d
rodrigo teixeira silva <r.teixeira1@outlook.com.br>
2023-07-25 18:45:37 -0300

    rotas-privadas

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import {  StatusBar, Text, View } from 'react-native';
+import { NativeBaseProvider } from 'native-base'
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Routes } from './src/routes';
+
+import { Loading } from '@components/loading';
+
+import { THEME } from './src/theme'
 
 export default function App() {
+ 
+    const [fontLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold }); 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={THEME}> 
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+    />
+
+      {fontLoaded ? <Routes/> : <Loading/>}
+    </NativeBaseProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
